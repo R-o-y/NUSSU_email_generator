5432fb4012507aea6a7d3772e5e2f4ed5156a56f
Luo, Yuyang <yuyluo@paypal.com>
2017-09-17 23:34:16 +0800

    implement save file logic

--- generate.js
+++ generate.js
@@ -1,11 +1,13 @@
 var fs = require('fs')
 var createHTML = require('create-html')
-var jsdom = require("jsdom");
+var jsdom = require('jsdom');
 var { JSDOM } = jsdom;
-var jquery = require("jquery");
+var jquery = require('jquery');
 
+const TEMPLATE_NAME = 'template.html';
+const OUTPUT_NAME = 'output_email.html';
 
-fs.readFile("test.html", 'utf8', function (err, data) {
+fs.readFile(TEMPLATE_NAME, 'utf8', function (err, data) {
     if (err) {
         throw err;
     } else {
@@ -15,18 +17,22 @@ fs.readFile("test.html", 'utf8', function (err, data) {
 
 function generateEmailFileFrom(data) {
     const dom = new JSDOM(data, { runScripts: "outside-only" });
-    
     var document = dom.window.document;
-
     var $ = jquery(dom.window);
 
-    console.log($("body").html())
+    // manipulate logic here
 
+
+    saveToFile($('html')[0].outerHTML);
     // saveToFile(document.documentElement.outerHTML);
 }
 
 function saveToFile(htmlContent) {
-    console.log(htmlContent)
+    console.log(htmlContent);
+    fs.writeFile(OUTPUT_NAME, htmlContent, function(err) {
+        if (err) throw err;
+        console.log('It\'s saved!');
+    });
 }
 
 
